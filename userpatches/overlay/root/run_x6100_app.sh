833f0ee9a38590ba94ebddc9f73a79592ae05400
Links <help.markus+gitk@gmail.com>
2022-04-14 12:28:08 +0200

    x6100 APP stop RF on service stop

--- userpatches/overlay/root/run_x6100_app.sh
+++ userpatches/overlay/root/run_x6100_app.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+function cleanup()
+{
+    echo 0 > /sys/class/gpio/gpio138/value
+}
+
+trap cleanup EXIT
+trap cleanup SIGINT
+
 echo 1 > /sys/class/gpio/gpio138/value
-chroot /mnt/x6100 /bin/bash -c 'source /etc/profile && /usr/app_qt/x6100_ui_v100'
+chroot /mnt/x6100 /bin/bash -c 'source /etc/profile && nice --5 /usr/app_qt/x6100_ui_v100'
